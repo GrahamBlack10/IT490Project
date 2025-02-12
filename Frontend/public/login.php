1a76a98ab0d3d839c958e832418f40efdd80aa0f
Yoen Jun Kim <yk296@njit.edu>
2025-02-12 00:01:22 -0500

    Setting up bootstrap

---
+++ Frontend/public/login.php
@@ -0,0 +1,16 @@
+<?php 
+include __DIR__ . "/../partials/header.php";
+include __DIR__ . "/../partials/nav.php"; 
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-012">
+                <h4>Login and Registration</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . "/../partials/footer.php"; ?>

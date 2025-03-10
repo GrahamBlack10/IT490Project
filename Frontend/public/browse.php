876cd0290c43ae366390b3d3ebf496e8106596d3
carlos-fuentes15 <144942611+carlos-fuentes15@users.noreply.github.com>
2025-03-10 02:58:57 -0400

    Update browse.php
    Updated file to be consistent with the other files

--- Frontend/public/browse.php
+++ Frontend/public/browse.php
@@ -4,8 +4,6 @@ include __DIR__ . "/../partials/nav.php";
 include __DIR__ . "/../lib/functions.php";
 
 
-
-// Redirect users who are not logged in
 if (!is_logged_in()) {
     header("Location: login.php");
     exit();
@@ -13,23 +11,19 @@ if (!is_logged_in()) {
 
 $searchQuery = isset($_GET['search']) ? trim($_GET['search']) : '';
 
-// Retrieve movies using your getMovies() function
+
 if (!isset($_GET['filter'])) {
     $movies = getMovies();
-}
-
-else if (isset($_GET['filter'])){
+} else if (isset($_GET['filter'])) {
     $movies = getMoviesWithFilter($_GET['filter']);
 }
 
-// Filter movies if a search term is provided
 
 if ($searchQuery !== '') {
     $movies = array_filter($movies, function($movie) use ($searchQuery) {
         return stripos($movie['title'], $searchQuery) !== false;
     });
 }
-
 ?>
 
 <body>
@@ -38,7 +32,7 @@ if ($searchQuery !== '') {
         <div class="row mb-4">
             <div class="col-md-8 offset-md-2">
                 <form method="GET" action="browse.php" class="input-group">
-                    <input type="text" name="search" class="form-control" placeholder="Search movies..." value="<?= htmlspecialchars($searchQuery) ?>">
+                    <input type="text" name="search" class="form-control" placeholder="Search movies..." value="<?php echo $searchQuery; ?>">
                     <button class="btn btn-danger" type="submit">Search</button>
                 </form>
             </div>
@@ -49,20 +43,17 @@ if ($searchQuery !== '') {
             <?php foreach($movies as $movie): ?>
                 <div class="col-md-4 mb-4">
                     <div class="card h-100">
-                        <img src="https://image.tmdb.org/t/p/w500<?= htmlspecialchars($movie['image']) ?>" class="card-img-top" alt="<?= htmlspecialchars($movie['title']) ?>">
+                        <img src="https://image.tmdb.org/t/p/w500<?php echo $movie['image']; ?>" class="card-img-top" alt="<?php echo $movie['title']; ?>">
                         <div class="card-body">
-                            <h5 class="card-title"><?= htmlspecialchars($movie['title']) ?></h5>
-                            <p class="card-text">Release Date: <?= htmlspecialchars($movie['releaseDate']) ?></p>
+                            <h5 class="card-title"><?php echo $movie['title']; ?></h5>
+                            <p class="card-text">Release Date: <?php echo $movie['releaseDate']; ?></p>
                         </div>
                         <div class="card-footer text-center">
-                            <a href="movie.php?tmdb_id=<?= urlencode($movie['tmdb_id']) ?>" class="btn btn-outline-danger btn-sm">More Info</a>
+                            <a href="movie.php?tmdb_id=<?php echo urlencode($movie['tmdb_id']); ?>" class="btn btn-outline-danger btn-sm">More Info</a>
                         </div>
-
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
     </div>
 </body>
-
-

c902fbfd80dfb773df8d322a0e2cb88d2e698c95
carlos-fuentes15 <144942611+carlos-fuentes15@users.noreply.github.com>
2025-03-10 03:01:18 -0400

    Update recommendations.php
    Styled the page

--- Frontend/public/recommendations.php
+++ Frontend/public/recommendations.php
@@ -6,32 +6,31 @@ require_once(__DIR__ . '/../rabbitmq/path.inc');
 require_once(__DIR__ . '/../rabbitmq/get_host_info.inc');
 require_once(__DIR__ . '/../rabbitmq/rabbitMQLib.inc');
 
-// Redirect users who are not logged in
+
 if (!is_logged_in()) {
     header("Location: login.php");
     exit();
 }
 
 $request = array();
-$request['type']= 'get_recommendations';
+$request['type'] = 'get_recommendations';
 $request['session_id'] = session_id();
 $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
 $response = $client->send_request($request);
-
 ?>
 
-<br>
-<p class="text-center">Here are some recommendations based on your favorite genre</p>
-
-<?php
-foreach($response as $movie) { ?>
-    <br>
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="https://image.tmdb.org/t/p/w500<?php echo $movie['image']?>" class="img-fluid rounded">
-    <div class="card-body">
-        <a href="https://www.themoviedb.org/movie/<?php echo $movie['tmdb_id']?>" class="btn btn-primary">Check it out!</a>
+<div class="container py-5">
+    <h3 class="text-center mb-4">Here are some recommendations based on your favorite genre</h3>
+    <div class="row">
+        <?php foreach($response as $movie): ?>
+            <div class="col-md-3 mb-4">
+                <div class="card h-100">
+                    <img src="https://image.tmdb.org/t/p/w500<?php echo $movie['image']; ?>" class="card-img-top img-fluid rounded" alt="<?php echo $movie['title']; ?>">
+                    <div class="card-body text-center">
+                        <a href="https://www.themoviedb.org/movie/<?php echo $movie['tmdb_id']; ?>" class="btn btn-primary">Check it out!</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-    </div>
-<?php
-}
-?>
+</div>

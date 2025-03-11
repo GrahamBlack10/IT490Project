0a1cec827ff0febf3efbaa498f0e06adce065b57
carlos-fuentes15 <144942611+carlos-fuentes15@users.noreply.github.com>
2025-03-11 00:57:44 -0400

    Update forum_post.php
    Styling of the forum_post.php file.

--- Frontend/public/forum_post.php
+++ Frontend/public/forum_post.php
@@ -7,7 +7,8 @@ require_once(__DIR__ . '/../rabbitmq/get_host_info.inc');
 require_once(__DIR__ . '/../rabbitmq/rabbitMQLib.inc');
 
 if (!is_logged_in()) {
-    die(header("Location: login.php"));
+    header("Location: login.php");
+    exit();
 }
 
 $id = (int)$_GET['id'];
@@ -23,50 +24,81 @@ $created = $response['created'];
 $description = $response['description'];
 ?>
 
-<br>
-<ul class="list-group">
-  <li class="list-group-item"><p class="font-weight-bold"><?php echo $title; ?></p></li>
-  <li class="list-group-item"><?php echo $user; ?></li>
-  <li class="list-group-item"><?php echo $created; ?></li>
-  <li class="list-group-item"><?php echo $description; ?></li>
-</ul>
-
-<br>
-<form action="forum_post.php?id=<?php echo $id; ?>" method="POST">
-    <div class="input-group">
-        <div class="input-group-prepend">
-            <span class="input-group-text">Comment</span>
+<div class="container py-5">
+  
+    <div class="row mb-4">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">
+                    <h4><?php echo $title; ?></h4>
+                </div>
+                <div class="card-body">
+                    <p><strong>Posted by:</strong> <?php echo $user; ?></p>
+                    <p><small class="text-muted">Posted on: <?php echo $created; ?></small></p>
+                    <p><?php echo $description; ?></p>
+                </div>
+            </div>
         </div>
-    <textarea class="form-control" name="comment" id="comment"></textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Add comment</button>
-</form>
+    
+    
+    <div class="row mb-4">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">
+                    Add Your Comment
+                </div>
+                <div class="card-body">
+                    <form action="forum_post.php?id=<?php echo $id; ?>" method="POST">
+                        <div class="mb-3">
+                            <label for="comment" class="form-label">Comment</label>
+                            <textarea class="form-control" name="comment" id="comment" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <?php
+    if (isset($_POST['comment'])) {
+        $request = array();
+        $request['type'] = 'create_forum_comment';
+        $request['comment'] = $_POST['comment'];
+        $request['forum_id'] = $id;
+        $request['session_id'] = session_id();
 
-<?php
-if (isset($_POST['comment'])) {
+        $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+        $response = $client->send_request($request);
+        header('Location: forum_post.php?id=' . $id);
+        exit;
+    }
+    
     $request = array();
-    $request['type'] = 'create_forum_comment';
-    $request['comment'] = $_POST['comment'];
-    $request['forum_id'] = $id;
-    $request['session_id'] = session_id();
-
+    $request['type'] = 'get_forum_comments';
+    $request['forum_id'] = $id; 
     $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
-    $response = $client->send_request($request);
-    header('Location: forum_post.php?id=' . $id);
-    exit;
-}
+    $comments = $client->send_request($request);
+    ?>
 
-$request = array();
-$request['type'] = 'get_forum_comments';
-$request['forum_id'] = $id; 
-$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
-$response = $client->send_request($request);
-
-foreach($response as $comment) {?>
-    <br>
-    <ul class="list-group">
-        <li class="list-group-item"><?php echo "Commenter: " . $comment['user']?></li>
-        <li class="list-group-item"><?php echo $comment['created']?></li>
-        <li class="list-group-item"><?php echo $comment['comment']?></li>
-    </ul>
-<?php } ?>
+   
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <h5 class="mb-3">Comments</h5>
+            <?php if (!empty($comments)): ?>
+                <?php foreach($comments as $comment): ?>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <p><strong>Commenter:</strong> <?php echo $comment['user']; ?></p>
+                            <p><small class="text-muted"><?php echo $comment['created']; ?></small></p>
+                            <p><?php echo $comment['comment']; ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No comments yet. Be the first to comment!</p>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>

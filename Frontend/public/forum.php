a7b51a441f3da2d73ba1486b348d1d09fcbe48c7
carlos-fuentes15 <144942611+carlos-fuentes15@users.noreply.github.com>
2025-03-11 00:56:56 -0400

    Update forum.php
    Styling of the forum.php file

--- Frontend/public/forum.php
+++ Frontend/public/forum.php
@@ -7,57 +7,83 @@ require_once(__DIR__ . '/../rabbitmq/get_host_info.inc');
 require_once(__DIR__ . '/../rabbitmq/rabbitMQLib.inc');
 
 if (!is_logged_in()) {
-    die(header("Location: login.php"));
+    header("Location: login.php");
+    exit();
 }
-
 ?>
 
-<br>
-<form action="forum.php" method="POST">
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-default">Title</span>
+<div class="container py-5">
+    <!-- Forum Creation Form -->
+    <div class="row mb-4">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">
+                    Create New Forum Post
+                </div>
+                <div class="card-body">
+                    <form action="forum.php" method="POST">
+                        <div class="mb-3">
+                            <label for="forum_title" class="form-label">Title</label>
+                            <input type="text" class="form-control" name="forum_title" id="forum_title">
+                        </div>
+                        <div class="mb-3">
+                            <label for="forum_description" class="form-label">Description</label>
+                            <textarea class="form-control" name="forum_description" id="forum_description" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Forum Post</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    <input type="text" class="form-control" name="forum_title" id="forum_title" aria-describedby="inputGroup-sizing-default">
     </div>
 
-    <div class="input-group">
-        <div class="input-group-prepend">
-            <span class="input-group-text">Description</span>
-        </div>
-    <textarea class="form-control" name="forum_description" id="forum_description"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Add forum</button>
-</form>
-
-<?php
-if (isset($_POST["forum_title"]) && isset($_POST["forum_description"])) {
-    $title = $_POST["forum_title"];
-    $description = $_POST["forum_description"];
-    $request = array();
-    $request['type'] = 'create_forum'; 
-    $request['title'] = $title;
-    $request['description'] = $description;
-    $request['session_id'] = session_id();
-    $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
-    $response = $client->send_request($request);
-
-    if ($response === 'success') {
-        echo 'Post created!';
+    <?php
+   
+    if (isset($_POST["forum_title"]) && isset($_POST["forum_description"])) {
+        $title = $_POST["forum_title"];
+        $description = $_POST["forum_description"];
+        $request = array();
+        $request['type'] = 'create_forum'; 
+        $request['title'] = $title;
+        $request['description'] = $description;
+        $request['session_id'] = session_id();
+        $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+        $response = $client->send_request($request);
+    
+        if ($response === 'success') {
+            echo '<div class="alert alert-success text-center" role="alert">Post created!</div>';
+        }
     }
-}
+    
+   
+    $request = array();
+    $request['type'] = 'get_forums';
+    $client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+    $forums = $client->send_request($request);
+    ?>
 
-$request = array();
-$request['type'] = 'get_forums';
-$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
-$response = $client->send_request($request);
-
-foreach ($response as $forum) { ?>
-    <br>
-    <ul class="list-group">
-        <li class="list-group-item"><p class="font-weight-bold"><a href="forum_post.php?id=<?php echo $forum['id']?>"><?php echo $forum['title']?></p></a></li>
-        <li class="list-group-item"><?php echo "Author: " . $forum['user']?></li>
-        <li class="list-group-item"><?php echo $forum['created']?></li>
-        <li class="list-group-item"><?php echo $forum['description']?><br/><br/></li>
-    </ul>
-<?php } ?>
+    
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <h3 class="mb-4 text-center">Forum Posts</h3>
+            <?php if (!empty($forums)): ?>
+                <?php foreach ($forums as $forum): ?>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="forum_post.php?id=<?php echo $forum['id']; ?>">
+                                    <?php echo $forum['title']; ?>
+                                </a>
+                            </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Author: <?php echo $forum['user']; ?></h6>
+                            <p class="card-text"><?php echo $forum['description']; ?></p>
+                            <p class="card-text"><small class="text-muted"><?php echo $forum['created']; ?></small></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p class="text-center">No forum posts found.</p>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>

3f5fd4c5c09310957a048089e857b1ba7bee8319
rhalliday <rah57@njit.edu>
2025-04-15 10:24:31 -0400

    Start of 2fa auth

--- Frontend/public/practice.php
+++ Frontend/public/practice.php
@@ -1,4 +1,6 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 include __DIR__ . "/../partials/header.php";
 include __DIR__ . "/../partials/nav.php"; 
 include __DIR__ . "/../lib/functions.php";
@@ -11,25 +13,46 @@ echo "Testing to see if I can get Username and User ID: " . PHP_EOL;
 $session_key = session_id();
 echo $session_key;
 
-$tmbd_id = '402431'; //Movie test
 
-$request = array();
-$request['type'] = 'get_movies';
-$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+$username = $_POST['username'];
+
+$client = new RabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+$request = ['type' => 'generate_2fa', 'username' => $username];
 $response = $client->send_request($request);
-echo "Movie: $response" . PHP_EOL; //FOR GETTING MOVIES ONLY
-var_dump($response);
+
+echo $response['message'];
 
 
-$request = array();
-$request['type'] = 'get_movie_details';
-$request['movie_id'] = $tmbd_id;
-$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
-$response = $client->send_request($request);
-echo "Movie Details: $response"; //FOR GETTING MOVIE DETAILS ONLY
-var_dump($response);
+
+
+
+
+
+
+//$tmbd_id = '402431'; //Movie test
+
+//$request = array();
+//$request['type'] = 'get_movies';
+//$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+//$response = $client->send_request($request);
+//echo "Movie: $response" . PHP_EOL; //FOR GETTING MOVIES ONLY
+//var_dump($response);
+
+
+//$request = array();
+//$request['type'] = 'get_movie_details';
+//$request['movie_id'] = $tmbd_id;
+//$client = new rabbitMQClient(__DIR__ . "/../rabbitmq/testRabbitMQ.ini", "testServer");
+//$response = $client->send_request($request);
+//echo "Movie Details: $response"; //FOR GETTING MOVIE DETAILS ONLY
+//var_dump($response);
 
 
 ?>
 
 
+<form action="/dmz/request_2fa.php" method="POST">
+  <label>Username:</label>
+  <input type="text" name="username" required>
+  <button type="submit">Send 2FA Code</button>
+</form>
